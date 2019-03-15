92c9c4a14d54f6263a9563f29b2928f1ae9dc480
Tonyjlum <tonyjlum@gmail.com>
2019-03-14 21:44:14 -0400

    fix bug on user with 0 repos

--- src/component/profileRender.js
+++ src/component/profileRender.js
@@ -6,12 +6,12 @@ const ProfileRender = ({currentUser}) => {
       <h1>{currentUser.login}</h1>
       {currentUser.name && <p>Name: {currentUser.name}</p>}
       {currentUser.bio && <p>Bio: {currentUser.bio}</p>}
-      {currentUser.blog && <a href={currentUser.blog} target="_blank"> {currentUser.blog}</a>}
-
+      {currentUser.blog && <a href={currentUser.blog} target="_blank"> Blog</a>}
       {currentUser.email && <p>Email: {currentUser.email}</p>}
       {currentUser.location && <p>Location: {currentUser.location}</p>}
       {currentUser.company && <p>Company: {currentUser.company}</p>}
-      {currentUser.public_repos && <p>Public Repos: {currentUser.public_repos}</p>}
+      {currentUser.public_repos > 0 && <p>Public Repos: {currentUser.public_repos}</p>}
+      <br/>
       <a href={currentUser.html_url} target="_blank"> Visit Github Page</a>
     </div>
   )
